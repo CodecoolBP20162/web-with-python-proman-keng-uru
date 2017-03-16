62355a08d62747225d9935a6dcb2580d88f148a1
Kata Meszaros <kata.meszaros92@gmail.com>
2017-03-16 15:53:20 +0100

    minor changes

--- static/card_storage.js
+++ static/card_storage.js
@@ -1,3 +1,30 @@
+    var new_cards = [];
+    var in_progress_cards = [];
+    var review_cards = [];
+    var done = [];
+
+
+$( function() {
+    $( "#sortable" ).sortable();
+    $( "#sortable" ).disableSelection();
+  } );
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    console.log(ev.target.id)
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    console.log(ev)
+    ev.target.appendChild(document.getElementById(data));
+}
+
+
 
 $(document).ready(function () {
     var cards = JSON.parse(localStorage.getItem("cards")) || []
