3cae4a80e73bb08fe1a0af097046861436a33e7b
Kata Meszaros <kata.meszaros92@gmail.com>
2017-03-16 14:17:21 +0100

    Dropping between columns id bug fixed

--- static/card_storage.js
+++ static/card_storage.js
@@ -1,15 +1,16 @@
 
 $(document).ready(function () {
-    var cards = JSON.parse(localStorage.getItem("cards")) || [];
+    var cards = JSON.parse(localStorage.getItem("cards")) || []
+    var cardId = 1;
     for (var i in cards) {
-        render_new_card(cards[i].title, cards[i].description);
-
+        render_new_card(cards[i].title, cards[i].description, cardId);
+        ++cardId;
     }
     $("#add-card").click(function () {
         //localStorage.setItem("cards", []);
         var cardName = $("#new-card-title").val();
         var cardDescription = $("#new-card-description").val();
-        var cardId = 1;
+
         cards = JSON.parse(localStorage.getItem("cards")) || cards;
         if (cardName  && !cards.includes(cardName)) {
             $("#new-card-title").val('');
@@ -18,17 +19,15 @@ $(document).ready(function () {
                 id: cardId,
                 title: cardName,
                 description:cardDescription
-                }
+                };
             cards.push(card);
             render_new_card(cardName, cardDescription, cardId);
-            console.log(cards);
             localStorage.setItem("cards", JSON.stringify(cards));
-            console.log(localStorage.getItem("cards"));
-            id += 1;
+            ++cardId;
         };
     });
 });
 
 var render_new_card = function(cardName, cardDescription, cardId){
-     $("#card_objects").append('<div class="card-text" id="' + cardId + '" draggable="true" ondragstart="drag(event)")><p>' + '<section class="card"> ' + '<header>' + cardName + '</header>' + '<article class="card_text">' + '<header>' + cardDescription + '</header>' + ' </article>' + '</section>' + '</p></div>');
+     $("#card_objects").append('<div class="card-text" id="card' + cardId + '" draggable="true" ondragstart="drag(event)")><p>' + '<section class="card"> ' + '<header>' + cardName + '</header>' + '<article class="card_text">' + '<header>' + cardDescription + '</header>' + ' </article>' + '</section>' + '</p></div>');
 }
