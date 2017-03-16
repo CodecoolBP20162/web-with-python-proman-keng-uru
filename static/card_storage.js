c1037091622dc91e0d6b34e52193304a7dd91d8e
Kata Meszaros <kata.meszaros92@gmail.com>
2017-03-16 13:47:18 +0100

    Dragging cards between columns works

--- static/card_storage.js
+++ static/card_storage.js
@@ -9,24 +9,26 @@ $(document).ready(function () {
         //localStorage.setItem("cards", []);
         var cardName = $("#new-card-title").val();
         var cardDescription = $("#new-card-description").val();
+        var cardId = 1;
         cards = JSON.parse(localStorage.getItem("cards")) || cards;
         if (cardName  && !cards.includes(cardName)) {
             $("#new-card-title").val('');
             $("#new-card-description").val('');
             var card = {
+                id: cardId,
                 title: cardName,
                 description:cardDescription
                 }
             cards.push(card);
-            render_new_card(cardName, cardDescription);
+            render_new_card(cardName, cardDescription, cardId);
             console.log(cards);
             localStorage.setItem("cards", JSON.stringify(cards));
             console.log(localStorage.getItem("cards"));
+            id += 1;
         };
     });
 });
 
-
-var render_new_card = function(cardName, cardDescription){
-     $("#card_objects").append('<div class="card-text sortable" draggable="true" ondragstart="drag(event)")><p>' + '<section class="card"> ' + '<header>' + cardName + '</header>' + '<article class="card_text">' + '<header>' + cardDescription + '</header>' + ' </article>' + '</section>' + '</p></div>');
+var render_new_card = function(cardName, cardDescription, cardId){
+     $("#card_objects").append('<div class="card-text" id="' + cardId + '" draggable="true" ondragstart="drag(event)")><p>' + '<section class="card"> ' + '<header>' + cardName + '</header>' + '<article class="card_text">' + '<header>' + cardDescription + '</header>' + ' </article>' + '</section>' + '</p></div>');
 }
--- static/script.js
+++ static/script.js
@@ -8,11 +8,13 @@ function allowDrop(ev) {
 }
 
 function drag(ev) {
+    console.log(ev.target.id)
     ev.dataTransfer.setData("text", ev.target.id);
 }
 
 function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
+    console.log(ev)
     ev.target.appendChild(document.getElementById(data));
 }
