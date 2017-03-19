6a12a680683e7c469cf44746557eb53888c051e9
Kata Meszaros <kata.meszaros92@gmail.com>
2017-03-19 12:37:57 +0100

    reconcile delete feature

--- static/card_storage.js
+++ static/card_storage.js
@@ -1,9 +1,7 @@
-
-
-$( function() {
-    $( "#sortable" ).sortable();
-    $( "#sortable" ).disableSelection();
-  } );
+$(function () {
+    $("#sortable").sortable();
+    $("#sortable").disableSelection();
+});
 function allowDrop(ev) {
     ev.preventDefault();
 }
@@ -16,64 +14,73 @@ function drag(ev) {
 function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
-    console.log(ev)
-    card_id = parseInt(data.substring(4));
-    if(ev.target.id === "delete_card"){
+    var card_id = parseInt(data.substring(4));
+    if (ev.target.id === "delete_card") {
         remove_card_from_db(card_id);
-    }else{
+        $('#' + data).remove();
+
+    } else if (isValidStatus(ev.target.id)) {
         change_card_status(card_id, ev.target.id)
         ev.target.appendChild(document.getElementById(data));
     }
 }
 
-
-
+function isValidStatus(statusId) {
+    var isFound = false;
+    $.each(Status, function(key, value){
+        if (value === statusId){
+            isFound = true;
+        }
+    });
+    return isFound;
+}
 
 var Status = {
-  NOT_YET_ARRANGED: "not_yet_arranged",
-  NEW : "new" ,
-  IN_PROGRESS: "in_progress",
-  REVIEW: "review",
-  DONE: "done"
+    NOT_YET_ARRANGED: "not_yet_arranged",
+    NEW: "new",
+    IN_PROGRESS: "in_progress",
+    REVIEW: "review",
+    DONE: "done"
 };
 
 
-function split_board_name(){
-    board_split = window.location.href.split('/');
-    board_name = board_split[board_split.length-1];
-   return board_name;
+function split_board_name() {
+    var board_split = window.location.href.split('/');
+    var board_name = board_split[board_split.length - 1];
+    return board_name;
 }
 
-function get_board_name(){
-   return "cards_of_" + split_board_name();
+function get_board_name() {
+    return "cards_of_" + split_board_name();
 }
 
 $(document).ready(function () {
     var cards = JSON.parse(localStorage.getItem(get_board_name())) || []
     var cardId = 1;
     for (var i in cards) {
-        render_new_card(cards[i].title, cards[i].description, cardId, cards[i].status);
-        ++cardId;
+        render_new_card(cards[i].title, cards[i].description, cards[i].id, cards[i].status);
+        cardId = Math.max(cardId, cards[i].id);
     }
 
     $(".nav").append("<li><a>" + decodeURI(split_board_name()) + "</a></li>>");
 
-        $("#add-card").click(function () {
+    $("#add-card").click(function () {
         //localStorage.setItem("cards", []);
         var cardName = $("#new-card-title").val();
         var cardDescription = $("#new-card-description").val();
 
         cards = JSON.parse(localStorage.getItem("cards")) || cards;
-        if (cardName  && !cards.includes(cardName)) {
+        if (cardName && !cards.includes(cardName)) {
             $("#new-card-title").val('');
             $("#new-card-description").val('');
+            ++cardId;
             add_new_card(cardId, cardName, cardDescription, Status.NOT_YET_ARRANGED)
             render_new_card(cardName, cardDescription, cardId, Status.NOT_YET_ARRANGED);
-            ++cardId;
-        };
+        }
+        ;
     });
 
-    $(".edit").click(function(){
+    $(".edit").click(function () {
         var card_name = $(this).closest(".card-text").find("header.cardname").html();
         var description = $(this).closest(".card-text").find("article").find("header").html();
         $('#new-card-title').val(card_name);
@@ -99,52 +106,51 @@ function save_card_handler() {
     $("#add-card").click(save_new_card_handler);
 }
 
-
 function save_new_card_handler() {
-        var cardName = $("#new-card-title").val();
-        var cardDescription = $("#new-card-description").val();
+    var cardName = $("#new-card-title").val();
+    var cardDescription = $("#new-card-description").val();
 
-        cards = JSON.parse(localStorage.getItem(get_board_name())) || cards;
-        if (cardName  && !cards.includes(cardName)) {
-            $("#new-card-title").val('');
-            $("#new-card-description").val('');
-            add_new_card(cardId, cardName, cardDescription, Status.NOT_YET_ARRANGED)
-            render_new_card(cardName, cardDescription, cardId, Status.NOT_YET_ARRANGED);
-            ++cardId;
-        };
+    cards = JSON.parse(localStorage.getItem(get_board_name())) || cards;
+    if (cardName && !cards.includes(cardName)) {
+        $("#new-card-title").val('');
+        $("#new-card-description").val('');
+        add_new_card(cardId, cardName, cardDescription, Status.NOT_YET_ARRANGED)
+        render_new_card(cardName, cardDescription, cardId, Status.NOT_YET_ARRANGED);
+        ++cardId;
+    };
 }
 
-var render_new_card = function(cardName, cardDescription, cardId, cardStatus){
-     $("#" + cardStatus).append('<div class="card-text" id="card' + cardId + '" draggable="true" ondragstart="drag(event)")><p>' + '<section class="card"> ' + '<header class="cardname">' + cardName + ' </header>' + '<article class="card_text description">' + '<header>' + cardDescription + ' </header>' + '<br><br><button class="edit">Edit card</button> </article>' + ' </section>' + '</p></div>');
+var render_new_card = function (cardName, cardDescription, cardId, cardStatus) {
+    $("#" + cardStatus).append('<div class="card-text" id="card' + cardId + '" draggable="true" ondragstart="drag(event)")><p>' + '<section class="card"> ' + '<header class="cardname">' + cardName + ' </header>' + '<article class="card_text description">' + '<header>' + cardDescription + ' </header>' + '<br><br><button class="edit">Edit card</button> </article>' + ' </section>' + '</p></div>');
 };
 
-function add_new_card(cardId, cardName, cardDescription, cardStatus){
+function add_new_card(cardId, cardName, cardDescription, cardStatus) {
     cards = JSON.parse(localStorage.getItem(get_board_name())) || [];
     var card = {
         id: cardId,
         title: cardName,
-        description:cardDescription,
-        status:cardStatus
-        };
+        description: cardDescription,
+        status: cardStatus
+    };
     cards.push(card);
     localStorage.setItem(get_board_name(), JSON.stringify(cards));
 }
 
-function remove_card_from_db(card_id){
+function remove_card_from_db(card_id) {
     cards = JSON.parse(localStorage.getItem(get_board_name())) || [];
-        for (i=0; i<cards.length; i++){
-            if (cards[i] === card_id){
-                cards.splice(i, 1);
-                break;
-            }
+    for (i = 0; i < cards.length; i++) {
+        if (cards[i].id === card_id) {
+            cards.splice(i, 1);
+            break;
         }
-        localStorage.setItem("cards", JSON.stringify(boards));
     }
+    localStorage.setItem(get_board_name(), JSON.stringify(cards));
+}
 
-function change_card_status(cardId, cardStatus){
+function change_card_status(cardId, cardStatus) {
     cards = JSON.parse(localStorage.getItem(get_board_name())) || [];
-    for (i=0; i<cards.length; i++){
-        if (cards[i].id === cardId){
+    for (i = 0; i < cards.length; i++) {
+        if (cards[i].id === cardId) {
             cards[i].status = cardStatus;
             break;
         }
@@ -153,10 +159,10 @@ function change_card_status(cardId, cardStatus){
 
 }
 
-function change_card(cardId, cardTitle, cardDescription){
+function change_card(cardId, cardTitle, cardDescription) {
     cards = JSON.parse(localStorage.getItem(get_board_name())) || [];
-    for (i=0; i<cards.length; i++){
-        if (cards[i].id === cardId){
+    for (i = 0; i < cards.length; i++) {
+        if (cards[i].id === cardId) {
             cards[i].title = cardTitle;
             cards[i].description = cardDescription;
             break;
@@ -166,7 +172,7 @@ function change_card(cardId, cardTitle, cardDescription){
 
 }
 
-function make_card_editable(ev){
+function make_card_editable(ev) {
     editable_card_id = ev.target.id;
     card_id = parseInt(editable_card_id.substring(4));
 }
