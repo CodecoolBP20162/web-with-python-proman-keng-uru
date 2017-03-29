754a2336c832630d62d1ab8f1fdab61e053ae892
Noncsi <lengyel.noemi96@gmail.com>
2017-03-29 14:15:06 +0200

    javascript modifying with delete bin

--- static/app.js
+++ static/app.js
@@ -5,25 +5,25 @@ const ul = document.getElementById('boards');
 
 
 function create_card_for_board(text) {
-  const list_item = document.createElement('li');
-  list_item.textContent = text;
-  const label = document.createElement('label');
+    const list_item = document.createElement('li');
+    list_item.textContent = text;
+    const label = document.createElement('label');
 
-  list_item.appendChild(label);
-  const editButton = document.createElement('button');
-  editButton.className = 'edit';
-  editButton.id = text;
-  list_item.appendChild(editButton);
+    list_item.appendChild(label);
+    const editButton = document.createElement('button');
+    editButton.className = 'edit';
+    editButton.id = text;
+    list_item.appendChild(editButton);
 
-  const removeButton = document.createElement('button');
-  removeButton.className = 'remove';
-  list_item.appendChild(removeButton);
+    const removeButton = document.createElement('button');
+    removeButton.className = 'remove';
+    list_item.appendChild(removeButton);
 
-  const hidden_board_id = document.createElement('input');
-  hidden_board_id.type = 'hidden';
-  hidden_board_id.value = text;
-  list_item.appendChild(hidden_board_id);
-  return list_item;
+    const hidden_board_id = document.createElement('input');
+    hidden_board_id.type = 'hidden';
+    hidden_board_id.value = text;
+    list_item.appendChild(hidden_board_id);
+    return list_item;
 
 }
 
@@ -41,28 +41,28 @@ form.addEventListener('submit', (e) => {
 
 
 ul.addEventListener('click', (e) => {
-  if (e.target.textContent === 'edit') {
-    window.location="/board/"+e.target.id;
-  };
+    if (e.target.className === 'edit') {
+        window.location = "/board/" + e.target.id;
+    };
 });
 
-function remove_board_from_db(board_id){
+function remove_board_from_db(board_id) {
     var boards = JSON.parse(localStorage.getItem("boards")) || [];
-        for (i=0; i<boards.length; i++){
-            if (boards[i] === board_id){
-                boards.splice(i, 1 );
-                break;
-            }
+    for (i = 0; i < boards.length; i++) {
+        if (boards[i] === board_id) {
+            boards.splice(i, 1);
+            break;
         }
-        localStorage.setItem("boards", JSON.stringify(boards));
     }
+    localStorage.setItem("boards", JSON.stringify(boards));
+}
 
 $(document).ready(function () {
 
 
     ul.appendChild(create_card_for_board("Go to mcdonalds ask for directions to burgerking"));
-        ul.appendChild(create_card_for_board("Walk into Sea World with a fishing pole"));
-        ul.appendChild(create_card_for_board("Sleeping"));
+    ul.appendChild(create_card_for_board("Walk into Sea World with a fishing pole"));
+    ul.appendChild(create_card_for_board("Sleeping"));
 
     var boards = JSON.parse(localStorage.getItem("boards")) || [];
     for (var i in boards) {
@@ -71,7 +71,7 @@ $(document).ready(function () {
     }
 
 
-    $('.remove').click(function(){
+    $('.remove').click(function () {
         $(this).closest('li').remove();
         li = $(this).closest('li')
         id = li.find('input').val();
